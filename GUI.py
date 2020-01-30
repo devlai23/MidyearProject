00f851bdb3c7c90d2833c97cc396e40f143bdddf
Devon <devonlai@gmail.com>
2020-01-30 08:15:05 -0500

    start of 1/30

--- GUI.py
+++ GUI.py
@@ -1,6 +1,7 @@
 from tkinter import *
 import random
 
+
 class Application(Frame):
     def __init__(self, master):
         super().__init__(master)
@@ -27,8 +28,24 @@ class Application(Frame):
         print(self.word)
         x = 0
         for char in self.word:
-            Label(self, text="_").grid(row=0, column=x+1)
+            Label(self, text="_", width=3, font=("Impact", 30)).grid(row=0, column=x + 1)
             x += 1
+        self.letterEntry = Entry(self)
+        self.letterEntry.grid(row=1, column=0, sticky=W)
+        confirmButton = Button(self, command=self.letterInput, text="Confirm")
+        confirmButton.grid(row=1, column=0, sticky=E)
+
+    def letterInput(self):
+        dict = {}
+        correct = []
+        for x in range(len(self.word)):
+            dict[x] = self.word[x]
+        input = self.letterEntry.get()
+        print(dict)
+        for x in range(len(dict)):
+            if input == dict[x]:
+                correct.append(x)
+        print
 
 root = Tk()
 root.title("Hangman")
