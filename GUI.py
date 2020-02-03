b0393ec26cec634f8b0db2838a43f71bf934c0f3
Devon <devonlai@gmail.com>
2020-02-03 08:07:11 -0500

    start of 2/3

--- GUI.py
+++ GUI.py
@@ -6,29 +6,30 @@ class Application(Frame):
     def __init__(self, master):
         super().__init__(master)
         self.grid()
-
+        self.picturenum = int(1)
         file = open("words.txt", "r")
         self.wordlist = []
+        self.times = 0
         for x in file:
             x = x.strip()
             self.wordlist.append(x)
-
         number = random.randint(1, len(self.wordlist))
         self.word = self.wordlist[number]
         self.wordlength = len(self.word)
-
         self.create_widgets()
 
     def create_widgets(self):
         picture = "Capture.PNG"
         imageLarge = PhotoImage(file=r"C:\Users\alvin\Pictures\Hangman\\" + picture)
-        w = Label(self, image=imageLarge)
-        w.photo = imageLarge
-        w.grid(row=0, column=0)
+        self.w = Label(self, image=imageLarge)
+        self.w.photo = imageLarge
+        self.w.grid(row=0, column=0)
         print(self.word)
         x = 0
+        self.labelDict = {}
         for char in self.word:
-            Label(self, text="_", width=3, font=("Impact", 30)).grid(row=0, column=x + 1)
+            self.labelDict[x] = Label(self, text="_", width=3, font=("Impact", 30))
+            self.labelDict[x].grid(row=0, column=x + 1)
             x += 1
         self.letterEntry = Entry(self)
         self.letterEntry.grid(row=1, column=0, sticky=W)
@@ -41,11 +42,27 @@ class Application(Frame):
         for x in range(len(self.word)):
             dict[x] = self.word[x]
         input = self.letterEntry.get()
-        print(dict)
+        input = input.lower()
         for x in range(len(dict)):
             if input == dict[x]:
                 correct.append(x)
-        print
+        if len(correct) == 0:
+            self.w.config(image='')
+            self.picturenum += 1
+            self.picturenum = str(self.picturenum)
+            picture = "Capture" + self.picturenum + ".PNG"
+            imageLarge = PhotoImage(file=r"C:\Users\alvin\Pictures\Hangman\\" + picture)
+            self.w = Label(self, image=imageLarge)
+            self.w.photo = imageLarge
+            self.w.grid(row=0, column=0)
+            self.picturenum = int(self.picturenum)
+        else:
+            for x in correct:
+                self.labelDict[x].config(text=dict[x])
+                self.times += 1
+            if self.times == len(self.word):
+                Label(self, text="You Won!", font=("Impact", 30)).grid(row=2, column=0, sticky=N)
+
 
 root = Tk()
 root.title("Hangman")
